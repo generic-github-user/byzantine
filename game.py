5cd7d98f40d880ce522d6a3f2fa239c754e32ce1
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-18 20:18:09 -0400

    add other imports

--- game.py
+++ game.py
@@ -1,3 +1,20 @@
 from direct.showbase.ShowBase import ShowBase
 from direct.showbase.DirectObject import DirectObject
 from direct.task import Task
+
+from panda3d.core import KeyboardButton
+
+from direct.gui.DirectGui import *
+from direct.interval.IntervalGlobal import *
+from panda3d.core import lookAt
+from panda3d.core import GeomVertexFormat, GeomVertexData
+from panda3d.core import Geom, GeomTriangles, GeomVertexWriter
+from panda3d.core import Texture, GeomNode
+from panda3d.core import PerspectiveLens
+from panda3d.core import CardMaker
+from panda3d.core import Light, Spotlight
+from panda3d.core import TextNode
+from panda3d.core import LVector3
+from pandac.PandaModules import WindowProperties
+import sys
+import os
